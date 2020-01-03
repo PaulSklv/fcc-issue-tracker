ae54e1fd90a80e849418942ed2588644db5743fb
Glitch (cobalt-blackberry) <none>
2020-01-03 14:39:01 +0000

    🎉🍦 Checkpoint ./server.js:9182067/904

--- server.js
+++ server.js
@@ -21,53 +21,56 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlPArser: true });
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const issues
+  const issuesCollection = client.db("issueTracker").collection("issues");
+  if(err) console.log("Data base error: " + err);
+  else {
+    console.log("Successful database connection.")
+  
+    //Sample front-end
+    app.route('/:project/')
+      .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/issue.html');
+      });
+
+    //Index page (static HTML)
+    app.route('/')
+      .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+      });
+
+    //For FCC testing purposes
+    fccTestingRoutes(app);
+
+    //Routing for API 
+    apiRoutes(app);  
+
+    //404 Not Found Middleware
+    app.use(function(req, res, next) {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
+    });
+
+    //Start our server and tests!
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("Listening on port " + process.env.PORT);
+      if(process.env.NODE_ENV==='test') {
+        console.log('Running Tests...');
+        setTimeout(function () {
+          try {
+            runner.run();
+          } catch(e) {
+            var error = e;
+              console.log('Tests are not valid:');
+              console.log(error);
+          }
+        }, 3500);
+      }
+    });
+  }
   
 })
-
-//Sample front-end
-app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
-  });
-
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
-});
-
 module.exports = app; //for testing
