be31ccf92abca2565a064af0e9f7f183e77ef64a
Glitch (cobalt-blackberry) <none>
2020-01-05 13:36:58 +0000

    🌨💷 Checkpoint ./tests/2_functional-tests.js:9182067/1131

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -29,11 +29,11 @@ suite('Functional Tests', function() {
         })
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.body.issue_title, "Title"),
-          assert.equal(res.body.issue_text, "text"),
-          assert.equal(res.body.created_by, "Functional Test - Every field filled in"),
-          assert.equal(res.body.assigned_to, "Chai and Mocha"),
-          assert.equal(res.body.status_text, "In QA")
+          assert.equal(res.body.issue_title, "Title");
+          assert.equal(res.body.issue_text, "text");
+          assert.equal(res.body.created_by, "Functional Test - Every field filled in");
+          assert.equal(res.body.assigned_to, "Chai and Mocha");
+          assert.equal(res.body.status_text, "In QA");
           //fill me in too!
           
           done();
@@ -41,11 +41,41 @@ suite('Functional Tests', function() {
       });
       
       test('Required fields filled in', function(done) {
-        
+        chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: 'Title',
+            issue_text: 'text',
+            created_by: 'Functional Test - Every field filled in',
+            assigned_to: 'Chai and Mocha',
+            status_text: 'In QA'
+          })
+          .end((err, res) => {
+            assert.isDefined(res.body.issue_title);
+            assert.isDefined(res.body.issue_text);
+            assert.isDefined(res.body.created_by);
+          
+            done();
+          })
       });
       
       test('Missing required fields', function(done) {
-        
+        chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: 'Title',
+            issue_text: 'text',
+            created_by: 'Functional Test - Every field filled in',
+            assigned_to: 'Chai and Mocha',
+            status_text: 'In QA'
+          })
+          .end((err, res) => {
+            assert.isUndefined(res.body.issue_title);
+            assert.isUndefined(res.body.issue_text);
+            assert.isUndefined(res.body.created_by);
+            
+            done();
+          })
       });
       
     });
