a44267918f9ba331d185baa9e7908794a285fba4
Glitch (cobalt-blackberry) <none>
2020-01-09 15:59:04 +0000

    📌⛽️ Checkpoint ./routes/api.js:9182067/778

--- routes/api.js
+++ routes/api.js
@@ -10,22 +10,32 @@
 
 var expect = require('chai').expect;
 const { MongoClient, ObjectID } = require('mongodb')
+const connection = MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = function (app, issuesCollection) {
   
   app.route('/api/issues/:project')
   
     .get(function (req, res) {
-      MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-          return console.log('Unable to connect to database!');
-        }
-
-        client.db('issueTracker').collection('issues').find(req.params.project).toArray((err, issues) => {
-          if(err) return console.log(err);
-          res.send(issues);
-        }); 
+      connection.then(client => {
+        client.db('issueTracker').collection('issues').find(req.params.project).toArray().then((result) => {
+          res.send(result);
+        }).catch(error => {
+          return console.log("Error was occured!", error)
+        })
+      }).catch(error => {
+        return console.log("Error was occured!", error)
       })
+//       MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//         if(error) {
+//           return console.log('Unable to connect to database!');
+//         }
+
+//         client.db('issueTracker').collection('issues').find(req.params.project).toArray((err, issues) => {
+//           if(err) return console.log(err);
+//           res.send(issues);
+//         }); 
+//       })
       // dataBaseCollection().find(req.query, (err, issues) => {
       //   if(err) console.log(err);
       //   else {
